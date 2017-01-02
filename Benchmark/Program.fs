6c44355fed0a756a4fbd39d36ac3390acbc21a30
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2017-01-02 21:07:11 +0100

    added mailbox processor logic -- still to test

--- Benchmark/Program.fs
+++ Benchmark/Program.fs
@@ -23,6 +23,47 @@ let measureTime task =
     Async.RunSynchronously task |> ignore
     (DateTime.Now - startTime).TotalMilliseconds
 
+// --- dispatching Agent
+type DispatchMsg =
+    | Begin of Uri * int
+    | WantWork of AsyncReplyChannel<Uri>
+    | DoneWork of TimeSpan
+
+let dispatchAgent = new MailboxProcessor<DispatchMsg>(fun inbox ->
+    let rec loop(url, todo, toreceive) = async {
+        let! msg = inbox.Receive()
+        match msg with
+        | Begin(uri, todo) ->
+            return! loop(uri, todo, todo)
+        | WantWork replyChannel when todo > 0  -> 
+            replyChannel.Reply(url)
+            return! loop(url, todo-1, toreceive)
+        | DoneWork timespan ->
+            //  TODO: process
+            return! loop(url, todo, toreceive-1)
+        | _ -> 
+            return! loop(url, todo, toreceive)
+    }
+    loop(null, 0, 0))
+
+dispatchAgent.Start()
+
+// -- downloading Agent
+let downloadAgent = new MailboxProcessor<Uri>(fun inbox -> 
+    let rec loop() = async {
+        let! uri = inbox.Receive()
+        let startTime = DateTime.Now
+        let! result = download uri
+        let duration = DateTime.Now - startTime
+        dispatchAgent.Post(DoneWork(duration))
+
+        return! loop()
+    }
+
+    loop())
+
+downloadAgent.Start()
+
 [<EntryPoint>]
 let main argv =
     let parser = ArgumentParser.Create<Arguments>()
@@ -32,6 +73,10 @@ let main argv =
     let uri = new Uri(results.GetResult(<@ Uri @>))
 
     printfn "Calling %A %d times, %d thread(s)" uri nrRequests nrThreads
+
+    // so sollte das gehen
+    // TODO: n DownloadAgents starten 
+    dispatchAgent.Post(Begin(uri, nrRequests))
 
     let executionTimes =
         uri
